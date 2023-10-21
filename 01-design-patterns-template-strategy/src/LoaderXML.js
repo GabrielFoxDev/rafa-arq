c47d8847bd57be2ac36683887fccb49956ac02e3
rafa-dev <rafaelgm.00@gmail.com>
2023-10-21 14:58:00 -0300

    segundo commit

--- 01-design-patterns-template-strategy/src/LoaderXML.js
+++ 01-design-patterns-template-strategy/src/LoaderXML.js
@@ -1,5 +1,4 @@
 import fs from 'node:fs';
-import xml2js from 'xml2js';
 import FileLoader from './FileLoader.js';
 
 export default class LoaderXML extends FileLoader {
@@ -11,32 +10,37 @@ export default class LoaderXML extends FileLoader {
     return new Promise((resolve, reject) => {
       const xmlData = fs.readFileSync(filename, 'utf-8');
 
-      // Use o xml2js para analisar o XML e converter em um objeto JavaScript (JSON).
-      const parser = new xml2js.Parser();
-      parser.parseString(xmlData, (error, result) => {
-        if (error) {
-          reject('Erro ao analisar XML: ' + error.message);
-        }
+      // Analise manualmente o XML e crie o JSON correspondente
+      const rows = xmlData.split('<row>');
 
-        // O resultado contém o objeto JavaScript que representa o XML.
-        // Neste exemplo, consideramos que o XML tem a estrutura que você forneceu.
-        // Você pode ajustar o código conforme necessário para a estrutura real do seu XML.
+      // Remove a primeira parte que não é necessária
+      rows.shift();
 
-        const jsonData = {
-          root: {
-            row: result.root.row.map((item) => {
+      const jsonData = {
+        root: {
+          row: rows.map((row) => {
+            const idMatch = /<ID>(.*?)<\/ID>/.exec(row);
+            const nomeMatch = /<Nome>(.*?)<\/Nome>/.exec(row);
+            const estadoMatch = /<Estado>(.*?)<\/Estado>/.exec(row);
+
+            if (idMatch && nomeMatch && estadoMatch) {
               return {
-                ID: item.ID[0],
-                Nome: item.Nome[0],
-                Estado: item.Estado[0],
+                ID: idMatch[1],
+                Nome: nomeMatch[1],
+                Estado: estadoMatch[1],
               };
-            }),
-          },
-        };
+            }
 
-        // Resolve a promessa com o objeto JSON gerado a partir do XML.
-        resolve(jsonData)
-      });
+            return null;
+          }),
+        },
+      };
+
+      // Filtrar os itens nulos que não corresponderam ao padrão
+      jsonData.root.row = jsonData.root.row.filter((item) => item !== null);
+
+      // Resolve a promessa com o objeto JSON gerado manualmente a partir do XML.
+      resolve(jsonData);
     });
   }
 }
