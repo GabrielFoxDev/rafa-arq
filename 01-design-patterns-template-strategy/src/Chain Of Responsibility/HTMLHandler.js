d911fa7543c4170dcdb4bb179d63e569cea79da7
Mateus <mateusbsanches@gmail.com>
2023-10-22 20:51:26 -0300

    Added chain of resposibility handlers

---
+++ 01-design-patterns-template-strategy/src/Chain Of Responsibility/HTMLHandler.js
@@ -0,0 +1,14 @@
+
+class HTMLHandler {
+    constructor(successor = null) {
+      this.successor = successor;
+    }
+  
+    handle(data) {
+      if (this.successor) {
+        this.successor.handle(data);
+      }
+    }
+}
+
+export default HTMLHandler;
