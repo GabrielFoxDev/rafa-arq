b95015de07c83b8fbcd82f525e47b9d3827676e3
rafa-dev <rafaelgm.00@gmail.com>
2023-10-22 21:22:24 -0300

    msg

--- 01-design-patterns-template-strategy/src/ChainOfResponsibility/StateHandler.js
+++ 01-design-patterns-template-strategy/src/ChainOfResponsibility/StateHandler.js
@@ -1,4 +1,4 @@
-import HTMLHandler from './src/Chain Of Responsibility/HTMLHandler.js'
+import HTMLHandler from './src/ChainOfResponsibility/HTMLHandler.js'
 
 class StateHandler extends HTMLHandler {
     handle(data) {
